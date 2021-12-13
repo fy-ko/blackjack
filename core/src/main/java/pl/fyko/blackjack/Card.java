3232c1eba042c9e079394ee513ab93226a7c2e31
Filip <f.kolodziejczyk1@gmail.com>
2021-12-13 14:50:46 +0100

    Added some JavaDoc for Card class

--- core/src/main/java/pl/fyko/blackjack/Card.java
+++ core/src/main/java/pl/fyko/blackjack/Card.java
@@ -3,6 +3,8 @@ package pl.fyko.blackjack;
 import java.util.Objects;
 
 /**
+ * Represents a single card and its value.
+ *
  * @author Filip Kołodziejczyk
  */
 class Card {
@@ -14,6 +16,13 @@ class Card {
         this.suit = suit;
     }
 
+    /**
+     * Returns the value of the card (set in {@link Figures}) while taking into account the ability of ACE card
+     * to have two different values depending on current cards value.
+     *
+     * @param currentValue value to take into account if the card is ACE
+     * @return value of the card
+     */
     int getValue(int currentValue) {
         if (figure.equals(Figures.ACE) && currentValue > 10) {
             return figure.value() - 10;
@@ -24,9 +33,15 @@ class Card {
 
     @Override
     public String toString() {
+        // TODO: change it to use ResourceBundle
         return String.format("%s of %s", figure, suit);
     }
 
+    /**
+     * Card is equal only to another card if both have the same figure and suit.
+     * @param o Object to check for equality
+     * @return true if cards have the same figure and suit
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
