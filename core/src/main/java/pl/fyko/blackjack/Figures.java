af6ba7feaf2a9150515c26e8468dd44bd9968eba
Filip <f.kolodziejczyk1@gmail.com>
2021-12-13 14:31:10 +0100

    Created enums for card Figures and Suits

---
+++ core/src/main/java/pl/fyko/blackjack/Figures.java
@@ -0,0 +1,30 @@
+package pl.fyko.blackjack;
+
+/**
+ * @author Filip Kołodziejczyk
+ */
+enum Figures {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
+    ACE(11);
+
+    private final int value;
+
+    Figures(int value) {
+        this.value = value;
+    }
+
+    int value() {
+        return value;
+    }
+}
---
+++ core/src/main/java/pl/fyko/blackjack/Suits.java
@@ -0,0 +1,11 @@
+package pl.fyko.blackjack;
+
+/**
+ * @author Filip Kołodziejczyk
+ */
+enum Suits {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+}
